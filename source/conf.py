f55e04043419f096f856d79d4a7e08bb07286fd8
yidao620c <yidao620@gmail.com>
2026-09-13 21:40:05 +0800

    添加 .readthedocs.yaml 配置文件，修复 Read the Docs 构建失败

--- source/conf.py
+++ source/conf.py
@@ -21,14 +21,9 @@
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
 import recommonmark
-from recommonmark.parser import CommonMarkParser
 from recommonmark.transform import AutoStructify
 import sphinx.environment
 from docutils.utils import get_source_line
-
-source_parsers = {
-    '.md': CommonMarkParser
-}
 
 source_suffix = ['.rst', '.md']
 
@@ -41,7 +36,7 @@ source_suffix = ['.rst', '.md']
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
